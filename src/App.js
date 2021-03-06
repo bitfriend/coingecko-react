f06a21e0785778b99aea71bcfa57ef34ff36b71a
bitfriend <bitfriend@hotmail.com>
2021-03-06 23:20:43 +0800

    Create react app

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { PureComponent } from 'react';
+import './App.css';
+
+class App extends PureComponent {
+  render = () => (
+    <div className="App">
+    </div>
+  )
+}
+
+export default App;
